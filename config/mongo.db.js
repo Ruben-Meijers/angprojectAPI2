cf271017c53624ebb56a71dc4aec2950f329baf9
Ruben Meijers <rubenmeijers18@gmail.com>
2017-12-13 07:40:34 +0100

    deleted obsolete

--- config/mongo.db.js
+++ config/mongo.db.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect(config.dburl);
 var connection = mongoose.connection
-    .once('open', () => console.log('Connected to Mongo on ' + config.dburl))
+    .once('open', () => console.log('connected to API'))
     .on('error', (error) => {
         console.warn('Warning', error.toString());
     });
